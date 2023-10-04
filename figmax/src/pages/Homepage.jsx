c05813cfb16ee68ff505828fc81995b8511173b5
shimingy <shimingy@uvic.ca>
2023-10-04 00:22:41 -0600

    layout and css implementation

--- figmax/src/pages/Homepage.jsx
+++ figmax/src/pages/Homepage.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
+import '../styles/Homepage.css';
+
+import logo from '../documentations/design/homepage/logo.svg';
+import button from '../documentations/design/homepage/button.svg';
+import menuicon from '../documentations/design/homepage/menuicon.svg';
+import pic from '../documentations/design/homepage/pic.svg';
 
 export default class Homepage extends Component {
   render() {
     return (
-      <div>Home
+      <div>
+        <div className='title'>
+          <img src={logo} alt="Logo" className='logo' />
+          <text className='productName'>FigMAX</text>
+          <button className='signUpButton'> Sign Up </button>
+          <button className='logInButton'> Log In </button>
+          <img src={menuicon} alt="Menu" className='menuicon' />
+        </div>
+        <div className='contents'>
+          <div className='slogan1'>
+            better idea <br />collaboration
+          </div>
+          <div className='slogan2'>
+            <br/> Create a whiteboard to share your ideas <br />and work with your friends
+          </div>
+        </div>
+        <div className='buttonArea'>
+          Enter the idea name here &nbsp;&nbsp;<img src={button} alt='button' classname='button'/>
+        </div>
+        <div className='pic'>
+          <img src={pic} alt='pic'/> 
+        </div>
       </div>
     )
   }
