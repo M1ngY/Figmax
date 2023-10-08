b1430fdaf51177eecce7bacca025737500313b5f
shimingy <shimingy@uvic.ca>
2023-10-08 02:19:37 -0600

    api get whiteboard name implementation

--- figmax/src/pages/Homepage.jsx
+++ figmax/src/pages/Homepage.jsx
@@ -7,6 +7,60 @@ import menuicon from '../documentations/design/homepage/menuicon.svg';
 import pic from '../documentations/design/homepage/pic.svg';
 
 export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ideaName: 'Enter the idea name here',
+      isFocused: false
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.handleInputBlur = this.handleInputBlur.bind(this);
+  }
+
+  setEditableDivRef = (element) => {
+    this.editableDiv = element;
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      ideaName: event.target.innerText
+    });
+  }
+
+  handleInputFocus() {
+    this.setState(prevState => ({
+      ideaName: prevState.ideaName === 'Enter the idea name here' ? '' : prevState.ideaName,
+      isFocused: true
+    }), () => {
+      const div = this.editableDiv;
+      // Ensure the cursor stays at the end when the div gains focus
+      const range = document.createRange();
+      range.selectNodeContents(div);
+      range.collapse(false);  // Collapse the range to the end
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+    });
+  }
+
+  handleInputBlur() {
+    if (!this.state.ideaName.trim()) {
+      this.setState({
+        ideaName: 'Enter the idea name here',
+        isFocused: false
+      });
+    }
+  }
+
+  async handleButtonClick() {
+    const { ideaName } = this.state;
+    const response = await fetch(`http://localhost:3001/whiteboard?boardName=${ideaName}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
@@ -26,7 +80,21 @@ export default class Homepage extends Component {
           </div>
         </div>
         <div className='buttonArea'>
-          Enter the idea name here &nbsp;&nbsp;<img src={button} alt='button' classname='button'/>
+          <div 
+            ref = {this.setEditableDivRef}
+            contentEditable="true" 
+            onFocus={this.handleInputFocus}
+            onBlur={this.handleInputBlur}
+            onInput={this.handleInputChange}
+            className='editableDiv'
+          >
+            {this.state.isFocused || this.state.ideaName !== 'Enter the idea name here' ? '' : this.state.ideaName}
+          </div>
+          <img 
+            src={button} 
+            alt='button' 
+            classname='button'
+            onClick={this.handleButtonClick}/>
         </div>
         <div className='pic'>
           <img src={pic} alt='pic'/> 
